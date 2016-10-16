cd5b95c27f59cc0932d07e1ddcc5f38d4cff9055
Porush Manjhi <Porush.unity@gmail.com>
2016-10-16 14:15:05 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -10,6 +10,14 @@ self.addEventListener('activate', function(event) {
 });
 
 self.addEventListener('push', function(event) {
-  console.log('Push message received', event);
-  // TODO
+  console.log('Push message', event);
+
+  var title = 'Push message';
+
+  event.waitUntil(
+    self.registration.showNotification(title, {
+     body: 'The Message',
+     icon: 'images/icon.png',
+     tag: 'my-tag'
+   }));
 });
