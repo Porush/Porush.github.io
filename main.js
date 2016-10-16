50270832b8984126560f2fe76a5306bf2619d8da
Porush Manjhi <Porush.unity@gmail.com>
2016-10-16 14:36:16 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,30 @@
+// Google Analytics Start
+(function(i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r;
+    i[r] = i[r] || function() {
+        (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date();
+    a = s.createElement(o),
+        m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m)
+})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+ga('create', 'UA-85743788-1', 'auto');
+ga('send', 'pageview');
+// Google Analytics End
+
+// OneSignal Start
+var OneSignal = window.OneSignal || [];
+OneSignal.push(["init", {
+    appId: "682fed89-588e-415c-be29-ed503be3d91f",
+    autoRegister: true,
+    /* Set to true to automatically prompt visitors */
+    subdomainName: 'porush-manjhi',
+    safari_web_id: '3145fc89-5d6b-4727-99ef-e9ab80472582',
+    notifyButton: {
+        enable: false /* Set to false to hide */
+    }
+}]);
+// OneSignal End
